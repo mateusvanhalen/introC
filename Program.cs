03b70896910cf227a0c54cc528bd025b2d3788f1
mateus <mateusvanhalen@gmail.com>
2018-10-03 11:29:54 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+
+        //one at at time here appartently
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> planetEnd = new List<string>(){"Uranus", "Neptune"};
+
+            planetList.AddRange(planetEnd);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Remove("Neptune");
+
+            // planetList.GetRange();
+
+            planetList.ForEach(planet => Console.WriteLine(planet));
+        }
+
+    }
+}
